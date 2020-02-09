e3f8e1fbf24aae650c1d9af91224b1017902912c
Tommy Crabtree <tommycrabtree@outlook.com>
2020-02-08 21:37:53 -0500

    Initial Commit

---
+++ NutritionApp.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using NutritionApp.API.Models;
+
+namespace NutritionApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options){}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
