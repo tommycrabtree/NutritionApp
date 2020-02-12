5987ea79bf56ab01b55685f48cac735423a4a915
Tommy Crabtree <tommycrabtree@outlook.com>
2020-02-12 05:33:50 -0500

    End of Section 5

--- NutritionApp.API/Controllers/AuthController.cs
+++ NutritionApp.API/Controllers/AuthController.cs
@@ -46,6 +46,7 @@ namespace NutritionApp.API.Controllers
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
             var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
+            
             if (userFromRepo == null)
                 return Unauthorized();
 
